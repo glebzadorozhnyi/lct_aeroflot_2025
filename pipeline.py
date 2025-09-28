14ffca155493a14d9a49429043c1086c64c8a670
Ilya <rasskazov.ilya.98@gmail.com>
2025-09-28 17:07:12 +0300

    add different types of image processing prior to CLF

--- pipeline.py
+++ pipeline.py
@@ -4,74 +4,68 @@ from typing import Sequence
 import cv2
 import numpy as np
 import onnxruntime as ort
+import torch
 from PIL import Image
 
+# from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
+# from sam2.build_sam import build_sam2
 # from sam2.sam2_image_predictor import SAM2ImagePredictor
 from tqdm import tqdm
-from ultralytics import YOLO
+from ultralytics import SAM, YOLO
 from ultralytics.engine.results import Boxes, Masks
 
 logging.basicConfig(level=logging.DEBUG)
-model = YOLO("models/yolo11m-seg.pt")
-
-# def process_by_sam2(img):
-#     predictor = SAM2ImagePredictor.from_pretrained("facebook/sam2-hiera-large")
-#     # image = Image.open(path).convert("RGB")
-#     w, h = img.size
-#     input_point = np.array([[w // 2, h // 2]])
-#     input_label = np.array([1])
-#     input_box = np.array([w // 4, h // 4, w // 4 + w // 2, h // 4 + h // 2])
-
-#     with torch.inference_mode(), torch.autocast("cuda", dtype=torch.bfloat16):
-#         predictor.set_image(np.array(img))
-#         masks, scores, logits = predictor.predict(
-#             point_coords=None,  # input_point,
-#             point_labels=input_label,
-#             box=input_box,
-#             multimask_output=False,
-#         )
-#     best_idx = int(np.argmax(scores)) if len(scores) > 0 else 0
-#     chosen_mask = masks[best_idx].astype(np.uint8)
-
-#     return chosen_mask
 
 
-def process_by_yolo_seg(img):
+# def process_by_sam2(img: Image.Image):
+#     sam2_checkpoint = "./models/sam2.1_hiera_tiny.pt"
+#     model_cfg = "configs/sam2.1/sam2.1_hiera_t.yaml"
+
+#     sam2_model = build_sam2(model_cfg, sam2_checkpoint, device="cpu")
+
+#     mask_generator = SAM2AutomaticMaskGenerator(sam2_model)
+
+#     with torch.inference_mode(), torch.autocast("cpu", dtype=torch.bfloat16):
+#         masks = mask_generator.generate(np.array(img))
+
+#     return masks
+
+
+def process_by_yolo(img: Image.Image):
+    model = YOLO("models/yolo11n.pt")
     results = model.predict(img, verbose=False, conf=0.2, device="cpu")
     logging.info(f"Detected {len(results)} objects")
     return results
 
 
-def pad2square(pil_image: Image.Image, pad_value: int = 0):
-    image = np.array(pil_image)
-    h, w = image.shape[:2]
-    if h == w:
-        return pil_image
-    size = max(h, w)
-    top = (size - h) // 2
-    left = (size - w) // 2
-    image = cv2.copyMakeBorder(
-        image,
-        top,
-        size - h - top,
-        left,
-        size - w - left,
-        cv2.BORDER_CONSTANT,
-        value=pad_value,
-    )
-    return Image.fromarray(image)
+def process_by_yolo_seg(img: Image.Image):
+    model = YOLO("models/yolo11m-seg.pt")
+    results = model.predict(img, verbose=False, conf=0.2, device="cpu")
+    logging.info(f"Detected {len(results)} objects")
+    return results
 
 
-def preprocess_for_clf(img: Image.Image, boxes: Boxes, segments: Masks):
-    padded_image = pad2square(img)
+def process_by_sam2_ultra(img: Image.Image):
+    model = SAM("models/sam2.1_t.pt")
+    results = model.predict(img, verbose=False, conf=0.2, device="cpu")
+    logging.info(f"Detected {len(results)} objects")
+    return results
 
+
+def process_by_fast_sam(img: Image.Image):
+    model = SAM("models/FastSAM-s.pt.pt")
+    results = model.predict(img, verbose=False, conf=0.2, device="cpu")
+    logging.info(f"Detected {len(results)} objects")
+    return results
+
+
+def preprocess_for_clf(img: Image.Image, boxes: Boxes):
     instruments = []
     # crop every object from image
     for box in boxes:
-        instrument = padded_image.crop(box.xyxy.tolist()[0])
+        instrument = img.crop(box.xyxy.tolist()[0])
         instrument_resized = instrument.resize((112, 112))
         instruments.append(instrument_resized)
-    # instrument.save("results.jpg")
     # TODO: use segments to crop masked object on white background
 
     return instruments
@@ -100,20 +94,26 @@ def process_by_clf(instruments: Sequence[Image.Image]):
     return probs
 
 
-def pipeline(img: Image):
-    results = process_by_yolo_seg(img)
+def pipeline(img: Image, type="yolo"):
+    processing_types = {
+        "yolo": process_by_yolo,
+        "yolo_seg": process_by_yolo_seg,
+        "sam2_ultra": process_by_sam2_ultra,
+        "fast_sam": process_by_fast_sam,
+    }
 
-    segments = results[0].masks
+    results = processing_types[type](img)
     boxes = results[0].boxes
-    annotated_image = results[0].plot()
-    Image.fromarray(annotated_image).save("anno_image1.jpg")
 
-    objects = preprocess_for_clf(img, boxes, segments)
+    annotated_image = results[0].plot()
+
+    objects = preprocess_for_clf(img, boxes)
     probs = process_by_clf(objects)
-    return probs
+    return probs, annotated_image
 
 
 if __name__ == "__main__":
-    # img = cv2.imread("image.jpg")
-    img = Image.open("image.jpg")
-    probs = pipeline(img)
+    img_path = "test_img_1.jpg"
+    img = Image.open(img_path)
+    probs, annotated_image = pipeline(img)
+    Image.fromarray(annotated_image).save(img_path.split(".")[0] + "proc.jpg")
